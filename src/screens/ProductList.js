1c80be91a6f27fa5a0657eadeaefd5899575aa08
ranganathan16 <mranganathan16@gmail.com>
2021-03-11 21:23:25 +0530

    delete product

--- src/screens/ProductList.js
+++ src/screens/ProductList.js
@@ -1,35 +1,78 @@
 import * as React from 'react';
-import { Text, View ,StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
-import { Avatar, Card, IconButton } from 'react-native-paper';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Alert
+} from 'react-native';
+import {Avatar, Card, IconButton} from 'react-native-paper';
 
 import {useDispatch, useSelector} from 'react-redux';
 import * as actions from '../store/actions/index';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function ProductList({navigation}) {
-  const productData = useSelector((state) => state.home.productlist);
+  const dispatch = useDispatch();
 
+  const productData = useSelector((state) => state.home.productlist);
+const deleteHandler=(data)=>{
+let tempProductlist=[...productData];
+tempProductlist=tempProductlist.filter(p=>p.id!==data.id);
+console.log([...tempProductlist]);
+dispatch(actions.setProductData([...tempProductlist]));
+        Alert.alert("Deleted sucessfully");
+
+
+
+}
 
   return (
-   
-      <View style={styles.container}>
-        <ScrollView style={styles.scroll}>
-        {
-          productData && productData.map((ele,index)=>{
-            return(
-              <Card  style={styles.card} key={index+'product'}>
-                <Card.Title title={ele.productName}  right={(props)=><TouchableOpacity style={styles.rightContent} onPress={()=>navigation.navigate('AddProduct',{editdata:ele})}><Text> <Icon name="edit" size={30} color="black" /> </Text></TouchableOpacity>}  />
-            </Card>
+    <View style={styles.container}>
+      {productData && productData.length === 0 ? (
+        <Text style={styles.empty}>No data found</Text>
+      ) : null}
+      <ScrollView style={styles.scroll}>
+        {productData &&
+          productData.map((ele, index) => {
+            return (
+              <Card style={styles.card} key={index + 'product'}>
+                <Card.Title
+                  title={ele.productName}
+                  right={(props) => (
+                    <View style={styles.rightContent}>
+                      <TouchableOpacity
+                        
+                        onPress={() =>
+                          navigation.navigate('AddProduct', {editdata: ele})
+                        }>
+                       
+                          <Icon name="edit" size={30} color="black" />
+                        
+                      </TouchableOpacity>
+                      <TouchableOpacity
+                        
+                        onPress={() =>
+                          deleteHandler(ele)
+                        }>
+                                          <Icon name="edit" size={30} color="red" />
+                       
+                      </TouchableOpacity>
+                    </View>
+                  )}
+                />
+              </Card>
             );
-          })
-        }     
+          })}
       </ScrollView>
 
-      <TouchableOpacity style={styles.addIcon} onPress={()=>navigation.navigate('AddProduct')} >
-      <Icon name="plus" size={30} color="black" />
+      <TouchableOpacity
+        style={styles.addIcon}
+        onPress={() => navigation.navigate('AddProduct')}>
+        <Icon name="plus" size={30} color="black" />
       </TouchableOpacity>
-      </View>
-
+    </View>
   );
 }
 
@@ -39,36 +82,33 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginTop: 20,
   },
-  card:{
-    marginBottom:8
+  card: {
+    marginBottom: 8,
   },
-  scroll:{
-    flex:1,
-    
+  scroll: {
+    flex: 1,
   },
-  rightContent:{
-    flex:1,
-    padding:3,
-    justifyContent:'center',
-    alignItems:'center',
-    marginHorizontal:3,
+  rightContent: {
+    flex: 1,
+    padding: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 3,
+    flexDirection:'row',
+   
   },
-  addIcon:{
-    position:'absolute',
-    bottom:50,
-    right:15,
-    backgroundColor:'grey',
-    width:60,
-    height:60,
-    borderRadius:30,
-    alignItems:'center',
-    justifyContent:'center',
-    
-
-
-  }
-
-
-
-  
+  addIcon: {
+    position: 'absolute',
+    bottom: 50,
+    right: 15,
+    backgroundColor: 'grey',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  empty: {
+    textAlign: 'center',
+  },
 });
